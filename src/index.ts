168abbeeecd9ccc4d7809d312986334416ef3baf
Jonny Nabors <jonnynabors@gmail.com>
2019-05-18 22:38:24 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Discord from "discord.js";
 import { Client } from "pg";
 import { App } from "./app";
-import DBL from "dblapi.js";
 const client = new Discord.Client();
 
 const pgClient = new Client({
@@ -11,7 +10,6 @@ pgClient.connect();
 
 client.login(process.env.CLIENT_ID);
 let app = new App(client, pgClient);
-let dblAPI = new DBL(process.env.DBL_API!, client);
 
 app.client.on("ready", () => {
   app.readyHandler();
