0b38c4163f142e74826120cafba93ee4c1f0c4c5
Jonny Nabors <jonnynabors@gmail.com>
2019-05-28 20:38:50 -0400

    Show drunk command as a RichEmbed

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,29 +1,41 @@
 import * as _ from "lodash";
+import { RichEmbed } from "discord.js";
+import Drink from "../models/Drink";
+const embed = new RichEmbed();
+// total drinks consumed by server
+let totalDrinks = 0;
+// amount of drinks that the drunkest person has had
+let highScore = 0;
 
-function messageFormatter(drinkData: any) {
+// TODO: Add a type
+let drinks: any[] = [];
+
+function messageFormatter(drinkData: any): RichEmbed {
+  embed.title = "Who's Drunk?";
   let msg = "";
-  for (var key in drinkData) {
-    var drinkCounts = _.countBy(drinkData[key], "drinkname");
-    msg += `${key} has had `;
-    let idx = 1;
-    for (var key in drinkCounts) {
-      if (drinkCounts.length === 1) {
-        msg += `a ${key}`;
-      } else {
-        if (drinkCounts[key] === 1) {
-          msg += `a ${key}`;
-        } else {
-          msg += `${drinkCounts[key]} ${key}s`;
-        }
-        if (idx < _.size(drinkCounts)) {
-          msg += `, and `;
-        }
-      }
-      idx++;
-    }
-    msg += `.\n`;
+  for (let key in drinkData) {
+    drinks.push(drinkData[key]);
+    totalDrinks += drinkData[key].length;
+    let currentDrinker = key;
+    msg = formatDrinksForUser(_.countBy(drinkData[key], "drinkname"));
+    embed.addField(`${currentDrinker} has had`, `${msg}`);
+    msg = "";
   }
-  return msg;
+
+  let mostDrunk = _.maxBy(drinks, drink => drink.length)[0].username;
+  embed.footer = {
+    text: `\nThat's a total of ${totalDrinks} drinks!`
+  };
+  embed.description = `Looks like **${mostDrunk}** has had the most to drink!`;
+  return embed;
+}
+
+function formatDrinksForUser(drinksByCount: _.Dictionary<Number>) {
+  let privateMessage = "";
+  for (let key in drinksByCount) {
+    privateMessage += `* ${drinksByCount[key]} ${key}\n`;
+  }
+  return privateMessage;
 }
 
 export { messageFormatter };
