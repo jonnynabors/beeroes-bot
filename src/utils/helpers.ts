04b55a962e8750015cb2c5816e5ea6f95383c47e
Jonny Nabors <jonnynabors@gmail.com>
2020-04-04 14:15:58 -0400

    Fix linting errors in helper.ts file

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,5 @@
-import * as _ from "lodash";
-import { RichEmbed } from "discord.js";
+import { maxBy, countBy } from 'lodash';
+import { RichEmbed } from 'discord.js';
 let embed: RichEmbed;
 // total drinks consumed by server
 let totalDrinks: number;
@@ -9,35 +9,9 @@ let drinks: any[];
 let highScore: number;
 let currentDrinker: string;
 
-function messageFormatter(drinkData: any): RichEmbed {
-  embed = new RichEmbed();
-  embed.setColor("RANDOM");
-  embed.setThumbnail("https://i.imgur.com/gaf3cVL.png");
-
-  currentDrinker = "";
-  totalDrinks = 0;
-  drinks = [];
-  highScore = 0;
-  let msg = "";
-  for (let key in drinkData) {
-    drinks.push(drinkData[key]);
-    totalDrinks += drinkData[key].length;
-
-    currentDrinker = key;
-    msg = formatDrinksForUser(_.countBy(drinkData[key], "drinkname"));
-
-    renderUsersDrinks(currentDrinker, msg);
-    msg = "";
-  }
-
-  renderDescription(_.maxBy(drinks, drink => drink.length));
-  embed.setFooter(`That's a total of ${totalDrinks} drinks!`);
-  return embed;
-}
-
-function formatDrinksForUser(drinksByCount: _.Dictionary<Number>) {
-  let privateMessage = "";
-  for (let key in drinksByCount) {
+function formatDrinksForUser(drinksByCount: _.Dictionary<number>) {
+  let privateMessage = '';
+  for (const key in drinksByCount) {
     privateMessage += `* ${drinksByCount[key]} ${key}\n`;
   }
   return privateMessage;
@@ -48,20 +22,44 @@ function renderUsersDrinks(user: string, drinks: string) {
   if (drinks.length < 1024) {
     embed.addField(`${user} has had`, `${drinks}`);
   } else {
-    embed.addField(
-      `${user} has had`,
-      `**more drinks than the server can count**`
-    );
+    embed.addField(`${user} has had`, `**more drinks than the server can count**`);
   }
 }
 
 function renderDescription(mostDrinksByUser: any) {
   if (mostDrinksByUser) {
-    let mostDrunk = mostDrinksByUser[0].username;
+    const mostDrunk = mostDrinksByUser[0].username;
     highScore = mostDrinksByUser.length;
     embed.setDescription(
       `Looks like **${mostDrunk}** has had the most to drink with ${highScore} total drinks!`
     );
   }
 }
+
+function messageFormatter(drinkData: any): RichEmbed {
+  embed = new RichEmbed();
+  embed.setColor('RANDOM');
+  embed.setThumbnail('https://i.imgur.com/gaf3cVL.png');
+
+  currentDrinker = '';
+  totalDrinks = 0;
+  drinks = [];
+  highScore = 0;
+  let msg = '';
+  for (const key in drinkData) {
+    drinks.push(drinkData[key]);
+    totalDrinks += drinkData[key].length;
+
+    currentDrinker = key;
+    msg = formatDrinksForUser(countBy(drinkData[key], 'drinkname'));
+
+    renderUsersDrinks(currentDrinker, msg);
+    msg = '';
+  }
+
+  renderDescription(maxBy(drinks, (drink) => drink.length));
+  embed.setFooter(`That's a total of ${totalDrinks} drinks!`);
+  return embed;
+}
+
 export { messageFormatter };
