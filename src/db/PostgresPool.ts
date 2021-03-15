3ba52af65b6f34cf69f512e7fef14603a548b7ea
Jonny Nabors <jnabors@ford.com>
2021-03-15 09:28:28 -0400

    Use SSL when connecting to postgres

--- src/db/PostgresPool.ts
+++ src/db/PostgresPool.ts
@@ -2,7 +2,7 @@ import { Pool, PoolClient } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.on('error', (err: Error, client: PoolClient) => {
